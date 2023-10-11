831e8a9a045f1f218ad4c9d7eb6b2f4d51df791c
Robert <dinizrobert2002@gmail.com>
2023-10-11 11:32:28 -0300

    Relacionamentos (Selos, Artistas, Discos)

--- app1/admin.py
+++ app1/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Disco
+from .models import Disco, Selo, Artista
 
 admin.site.register(Disco)
+admin.site.register(Selo)
+admin.site.register(Artista)
+
--- app1/models.py
+++ app1/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+class Selo(models.Model):
+    nome_produtora = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nome_produtora
+
+class Artista(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
 class Disco(models.Model):
     nome = models.CharField(max_length=50)
-    descricao = models.TextField()
-    nome_produtora = models.CharField(max_length=50)
+    descricao = models.TextField(max_length=250)
+    selo = models.ForeignKey(Selo, on_delete=models.CASCADE, blank=True, null=True)
     ano = models.DateField()
     pais = models.CharField(max_length=50)
     GENENEROS_MUSICAIS = [
@@ -14,7 +25,8 @@ class Disco(models.Model):
     ]
     generos = models.CharField(max_length=30, choices=GENENEROS_MUSICAIS, default="A")
     quantidade = models.IntegerField()
+    artistas = models.ManyToManyField(Artista, related_name="discos")
 
     def __str__(self):
-        return f'{self.nome} - {self.nome_produtora}'
+        return f'{self.nome} - {self.selo}'
 
